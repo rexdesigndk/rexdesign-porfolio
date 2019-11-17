c5432d7f7ef4de73c514168cee421f53b5e0f9b4
Jesper <jesperex@gmail.com>
2019-11-18 00:41:43 +0100

    + Added all sections + Almost done with styling + Laravel mix setup done + Tailwind config added + Added custom images

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,21 @@
 module.exports = {
   theme: {
+    fontFamily: {
+     'body': ['Roboto', 'sans-serif'],
+          },
     screens: {
       'xl': '1440px'
     },
     container: {
       center: true,
     },
-    extend: {}
+    extend: {
+      colors: {
+        'primary': '#0029FF',
+        'secondary': '#ececeb',
+        'accent': '#e25822',
+      }
+    }
   },
   variants: {},
   plugins: []
