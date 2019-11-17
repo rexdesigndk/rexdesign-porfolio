c4a5ffd91d020d37b6ec85e7885bbeb4e34fdddd
Jesper <jesperex@gmail.com>
2019-11-17 02:59:41 +0100

    Initial release

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  theme: {
+    screens: {
+      'xl': '1440px'
+    },
+    container: {
+      center: true,
+    },
+    extend: {}
+  },
+  variants: {},
+  plugins: []
+}
---
+++ webpack.mix.js
@@ -0,0 +1,19 @@
+let mix = require("laravel-mix");
+
+mix
+  .js("src/app.js", "dist/")
+  .postCss("src/main.css", "dist/", [
+    require("tailwindcss"),
+    require("autoprefixer"),
+    // require("@fullhuman/postcss-purgecss")({
+    //   content: ["index.html"],
+    //   from: ["/src/css/main.css"],
+    //   defaultExtractor: content => content.match(/[A-Za-z0-9-_:\/]+/g) || []
+    // })
+  ])
+  .options({
+    clearConsole: true,
+    cssNano: {
+      discardComments: { removeAll: true }
+    }
+  });
