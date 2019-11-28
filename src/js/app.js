e6bd6390471b0f6e24da40c370b0932d8dd4d0e1
Jesper <jesperex@gmail.com>
2019-11-28 01:12:59 +0100

    + Still not fixed

--- src/js/app.js
+++ src/js/app.js
@@ -6,15 +6,15 @@ var myLazyLoad = new LazyLoad({
   load_delay: 100
 });
 
-// AOS.init({
-//   disable: window.innerWidth < 768,
-//   disable: window.matchMedia('(prefers-reduced-motion: reduce)').matches,
-//   easing: 'ease-in-out-cubic',
-//   delay: 300,
-//   once: true,
-//   duration: 800,
-//   anchorPlacement: 'top-center'
-// });
+AOS.init({
+  disable: window.innerWidth < 768,
+  disable: window.matchMedia('(prefers-reduced-motion: reduce)').matches,
+  easing: 'ease-in-out-cubic',
+  delay: 300,
+  once: true,
+  duration: 800,
+  anchorPlacement: 'top-center'
+});
 
 let menuToggle = document.getElementById("menuToggle")
 let menu = document.getElementById("menu")
