755f2391153e10ca22216c2491c3d650a87a63a8
Jesper <jesperex@gmail.com>
2019-11-29 17:58:24 +0100

    ran prod script

--- webpack.mix.js
+++ webpack.mix.js
@@ -5,11 +5,11 @@ mix
   .postCss("src/css/main.css", "dist/", [
     require("tailwindcss"),
     require("autoprefixer"),
-    // require("@fullhuman/postcss-purgecss")({
-    //   content: ["index.html", "404.html"],
-    //   from: ["src/css/main.css"],
-    //   defaultExtractor: content => content.match(/[A-Za-z0-9-_:\/]+/g) || []
-    // })
+    require("@fullhuman/postcss-purgecss")({
+      content: ["index.html", "404.html", "success.html"],
+      from: ["src/css/main.css"],
+      defaultExtractor: content => content.match(/[A-Za-z0-9-_:\/]+/g) || []
+    })
   ])
   .options({
     clearConsole: true,
